2f805313b9934800eb5c5b837fa2d320827e1dd6
Burence1 <burensomondi@gmail.com>
2021-05-20 14:08:37 +0300

    add images class

--- insta/models.py
+++ insta/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+from django.db.models.fields import DateField
 
 # Create your models here.
+class Images(models.Model):
+  image = CloudinaryField('photo')
+  caption=models.TextField(blank=True)
+  name=models.CharField(max_length=30)
+  pub_date=models.DateField(auto_now_add=True)
+  
+  
--- instagram/settings.py
+++ instagram/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import cloudinary
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -39,7 +40,14 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'insta.apps.InstaConfig'
     'bootstrap5',
+    'cloudinary',
 ]
+
+cloudinary.config(
+    cloud_name="burens",
+    api_key="727223526185189",
+    api_secret="i5QqXId24yed4oDPoFnfOmVStKQ",
+)
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
