bfe0f3466cac4e7ca97455048b712bf0c19c28d4
Burence1 <burensomondi@gmail.com>
2021-05-21 13:11:06 +0300

    add display current user's posts on timeline logic

--- insta/views.py
+++ insta/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.http.response import Http404
+from django.shortcuts import render,redirect
+from django.http import HttpResponseRedirect,Http404
+from .models import Profile,Follow,Image,Comments
+from django.contrib.auth.models import User
 
 # Create your views here.
+def index(request):
+  title="Instagramex"
+  current_user=request.user
+  try:
+    profile= Profile.objects.get(user=current_user)
+  except Profile.DoesNotExist:
+    raise Http404
+
+  index_timeline=[]
+  images = Image.profile_images(profile=profile)
+  for image in images:
+    index_timeline.append(image.id)
+
+  followers_posts=Follow.objects.filter(follower=profile)
+  for follower in followers_posts:
+    
